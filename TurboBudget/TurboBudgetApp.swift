3418fa6d08d1aacce72eb9516da75307dacb2bc8
KaayZenn <theosem46@gmail.com>
2023-06-15 17:52:46 +0200

    Initial Commit

---
+++ TurboBudget/TurboBudgetApp.swift
@@ -0,0 +1,20 @@
+//
+//  TurboBudgetApp.swift
+//  TurboBudget
+//
+//  Created by Théo Sementa on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TurboBudgetApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
