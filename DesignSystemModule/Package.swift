5c2401fc1b6de88250c7479a3a89afdce3d685df
Theo Sementa <theosementa@gmail.com>
2025-07-17 20:56:52 +0200

    feat: add DesignSystemModule

---
+++ DesignSystemModule/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DesignSystemModule",
+    platforms: [.iOS(.v16)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DesignSystemModule",
+            targets: ["DesignSystemModule"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "DesignSystemModule"),
+        .testTarget(
+            name: "DesignSystemModuleTests",
+            dependencies: ["DesignSystemModule"]
+        ),
+    ]
+)
